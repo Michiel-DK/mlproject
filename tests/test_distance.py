fff1a21cee9338852a55a6c604619e0bf42fe7ff
Michiel De Koninck <michieldekoninck2@gmail.com>
2022-05-23 11:33:32 +0100

    update test

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,7 +1,9 @@
 from mlproject.distance import haversine
 
+#test haversine
 def test_haversine_outcome():
     assert haversine(48.865070, 2.380009, 48.235070, 2.393409) == 70.00789153218594
     
+#test len
 def test_haversine_len():
     assert type(haversine(48.865070, 2.380009, 48.235070, 2.393409)) == float
